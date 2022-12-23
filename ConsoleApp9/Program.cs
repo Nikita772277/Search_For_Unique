17c4ec255fa33c0cc8a61182cdbac437ac3a16fd
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-23 12:35:05 +0300

    123

--- ConsoleApp9/Program.cs
+++ ConsoleApp9/Program.cs
@@ -2,9 +2,9 @@
 using ConsoleApp9;
 
 WorkingWithStrings wws=new();
-string[] strings = new string[] { "Aa", "aaa", "aaaaa", "BbBb", "Aaaa", "aaaaaaa", "a" };
+string[] strings = new string[] { "BbBb", "aaa", "aaaaa", "aA", "Aaaa", "aaaaaaa", "a" };
 string[] strings2 = new string[] {""};
-var unique= wws.SearchForAUnique(strings2);
+var unique= wws.SearchForAUnique(strings);
 if (unique != null)
 {
     foreach (string s in unique)
--- ConsoleApp9/WorkingWithStrings.cs
+++ ConsoleApp9/WorkingWithStrings.cs
@@ -15,17 +15,17 @@ namespace ConsoleApp9
                 return null;
             }
             List<string> list = new List<string>();
-            foreach( string s in strings)
+            for(int s = 0; s < strings.Length; s++)
             {
                 string word = strings[0];
                 int counter = 0;
-                for (int i = 0; i < s.Length; i++)
+                for (int i = 0; i < s; i++)
                 {
                     for (int j = 0; j < word.Length; j++)
                     {
-                        if (s[i] == word[j])
+                        if (strings[s][i] == word[j])
                         {
-                            if (s[i] != ' ')
+                            if (strings[s][i] != ' ')
                                 counter++;
                         }
                         if (counter == 1)
@@ -36,7 +36,7 @@ namespace ConsoleApp9
                 }
                 if(counter == 0)
                 {
-                    list.Add(s);
+                    list.Add(strings[s]);
                 }
                 if (list.Count == 1)
                 {
