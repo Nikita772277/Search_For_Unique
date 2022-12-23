070fd1b2808ff8ec12cf5e92944882ef15606d55
Студент 3 < 3@DESKTOP-PS97MJ6>
2022-12-23 10:35:15 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using ConsoleApp9;
+
+WorkingWithStrings wws=new();
+string[] strings = new string[] { "Aa", "aaa", "aaaaa", "BbBb", "Aaaa", "aaaaaaa", "a" };
+string[] strings2 = new string[] {""};
+var unique= wws.SearchForAUnique(strings2);
+if (unique != null)
+{
+    foreach (string s in unique)
+        Console.WriteLine(s);
+}
---
+++ ConsoleApp9/WorkingWithStrings.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class WorkingWithStrings
+    {
+        public List<string> SearchForAUnique(string[] strings)
+        {
+            if (strings == null)
+            {
+                return null;
+            }
+            List<string> list = new List<string>();
+            foreach( string s in strings)
+            {
+                string word = strings[0];
+                int counter = 0;
+                for (int i = 0; i < s.Length; i++)
+                {
+                    for (int j = 0; j < word.Length; j++)
+                    {
+                        if (s[i] == word[j])
+                        {
+                            if (s[i] != ' ')
+                                counter++;
+                        }
+                        if (counter == 1)
+                        {
+                            break;
+                        }
+                    }
+                }
+                if(counter == 0)
+                {
+                    list.Add(s);
+                }
+                if (list.Count == 1)
+                {
+                    break;
+                }
+            }
+            return list;
+        }
+    }
+}
